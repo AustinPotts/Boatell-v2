8ef62cf67dc84bdff3b87611716a6e820fd126b8
AustinPotts <pottsevolvements@gmail.com>
2020-03-06 21:28:51 -0500

    Configured Project

---
+++ Boatell-x-v2/Boatell-x-v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boatell-x-v2
+//
+//  Created by Austin Potts on 3/6/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

27ffac5a36c6236f175bac6d8ea608d2624c3cba
AustinPotts <pottsevolvements@gmail.com>
2020-03-06 22:06:44 -0500

    Login View Controller Configured

---
+++ Boatell-x-v2/Boatell-x-v2/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  Boatell-x-v2
+//
+//  Created by Austin Potts on 3/6/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loginButton.layer.cornerRadius = 20
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

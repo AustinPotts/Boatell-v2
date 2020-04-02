5e2122a277535f4cc1723e3b5bfe44774158e534
AustinPotts <pottsevolvements@gmail.com>
2020-04-01 23:12:05 -0400

    Configured Layout for FSCalendar User Schedule

--- Boatell-x-v2/Boatell-x-v2/User View Controllers/UserCalendarViewController.swift
+++ Boatell-x-v2/Boatell-x-v2/User View Controllers/UserCalendarViewController.swift
@@ -11,12 +11,31 @@ import FSCalendar
 
 class UserCalendarViewController: UIViewController {
 
+    fileprivate weak var calendar: FSCalendar!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
+        calendar.register(FSCalendarCell.self, forCellReuseIdentifier: "CELL")
+        calendar.dataSource = self
+        calendar.delegate = self
+        calendar.backgroundColor = .white
+        calendar.layer.cornerRadius = 20
+        calendar.layer.shadowRadius = 20
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(calendar)
+        self.calendar = calendar
+        
+        calendar.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -230).isActive = true
+      //  calendar.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+        calendar.heightAnchor.constraint(equalToConstant: 355).isActive = true
+        calendar.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
     }
-    
+
 
     /*
     // MARK: - Navigation
@@ -27,5 +46,18 @@ class UserCalendarViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+
+// MARK: - FSCalendar Delegation
+
+extension UserCalendarViewController: FSCalendarDataSource, FSCalendarDelegate {
+
+    func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
+        let cell = calendar.dequeueReusableCell(withIdentifier: "CELL", for: date, at: position)
+        
+        return cell
+    }
 
 }

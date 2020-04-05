e65fd4699d29d7207b3aeabdb0db6e22b966240f
AustinPotts <pottsevolvements@gmail.com>
2020-04-04 22:33:34 -0400

    Configured User Model + Successfully Fetching Users to TVC

---
+++ Boatell-x-v2/Boatell-x-v2/Model/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  Boatell-x-v2
+//
+//  Created by Austin Potts on 4/4/20.
+//  Copyright © 2020 Potts Evolvements. All rights reserved.
+//
+
+import Foundation
+
+
+ class User: NSObject {
+    
+    
+   @objc var name: String?
+   @objc var email: String?
+    
+    
+}
+
+
+//struct UserResult: Decodable {
+//    let results: [User]
+//}
+//
+//struct User: Decodable{
+//    var name: Name
+//    var email: String
+//    var phone: String
+//    var picture: Picture
+//}
